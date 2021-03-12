a141ba4cf807b6548b46ceabcad622f41f965f6c
pabloitu_home <pciturri@gfz-potsdam.de>
2021-03-12 14:19:08 +0100

    Added participants ETH

--- organizational/create_pdf.sh
+++ organizational/create_pdf.sh
@@ -3,5 +3,5 @@
 
 cat invitation.svg | inkscape --pipe --export-filename=invitation.pdf
 cat agenda.svg | inkscape --pipe --export-filename=agenda.pdf
-pdfunite invitation.pdf agenda.pdf CSEPworkshop_invitation.pdf
+pdfunite invitation.pdf agenda.pdf RISE_CSEP_Workshop_invitation.pdf
 rm invitation.pdf agenda.pdf
